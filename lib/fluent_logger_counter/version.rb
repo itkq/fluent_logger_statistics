7128a36381b8116f296ebe709874dd3debaa17a9
Takuya Kosugiyama <takuya-kosugiyama@cookpad.com>
2017-01-17 14:47:56 +0900

    :tada: Create gem skelton

---
+++ lib/fluent_logger_counter/version.rb
@@ -0,0 +1,3 @@
+module FluentLoggerCounter
+  VERSION = "0.1.0"
+end
---
+++ spec/fluent_logger_counter_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe FluentLoggerCounter do
+  it "has a version number" do
+    expect(FluentLoggerCounter::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
