56b845bfc00382d3ce9896f89a49112cade30b54
Takuya Kosugiyama <takuya-kosugiyama@cookpad.com>
2017-01-27 11:44:56 +0900

    :sparkles: Multi Logger

--- lib/fluent_logger_counter/middleware.rb
+++ lib/fluent_logger_counter/middleware.rb
@@ -2,17 +2,18 @@ require "fluent_logger_counter/app"
 
 module FluentLoggerCounter
   class Middleware
-    def initialize(app, path, logger)
+    def initialize(app, endpoint, loggers)
       @app = app
-      @path = path
-      @fluent_app = App.new(logger)
+      @fluent_apps = loggers.map{|resource, logger|
+        [ (Pathname.new(endpoint)+resource.to_s).to_s, App.new(logger) ]
+      }.to_h
     end
 
     ACCEPT_METHODS = ['GET'].freeze
 
     def call(env)
-      if env['PATH_INFO'] == @path && ACCEPT_METHODS.include?(env['REQUEST_METHOD'])
-        @fluent_app.call(env)
+      if @fluent_apps[env['PATH_INFO']] && ACCEPT_METHODS.include?(env['REQUEST_METHOD'])
+        @fluent_apps[env['PATH_INFO']].call(env)
       else
         @app.call(env)
       end
