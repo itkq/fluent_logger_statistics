d3f76ee413c4370740871db5c16e36492ec7b917
Takuya Kosugiyama <takuya-kosugiyama@cookpad.com>
2017-01-27 11:44:56 +0900

    :sparkles: Multi Logger

--- spec/middleware_spec.rb
+++ spec/middleware_spec.rb
@@ -10,7 +10,8 @@ end
 describe 'middleware' do
   include Rack::Test::Methods
 
-  let(:logger) { Logger.new(STDOUT) }
+  let(:logger1) { Logger.new(STDOUT) }
+  let(:logger2) { Logger.new(STDERR) }
   let(:nextapp) { -> (env) { [200, {'Content-Type' => 'text/plain'}, ['OK']] } }
   let(:mockapp) do
     Class.new do
@@ -23,7 +24,7 @@ describe 'middleware' do
         self.class.instances << self
       end
 
-      attr_reader :logger, :options
+      attr_reader :logger
 
       def call(env)
         [200, {'Content-Type' => 'application/json'}, ['{"buffer_size":0}']]
@@ -34,7 +35,11 @@ describe 'middleware' do
   end
 
   let(:app) {
-    FluentLoggerCounter::Middleware.new(nextapp, '/api/fluentcounter/test_logger', logger)
+    FluentLoggerCounter::Middleware.new(
+      nextapp,
+      '/api/fluent_logger_counter/',
+      {"stdout" => logger1, "stderr" => logger2}
+    )
   }
 
   before do
@@ -50,8 +55,10 @@ describe 'middleware' do
   it 'instantiates App with proper argument' do
     app
 
-    expect(mockapp.instances.size).to be_eql 1
-    expect(mockapp.instances.first.logger).to be_eql logger
+    expect(mockapp.instances.size).to be_eql 2
+    puts mockapp.instances
+    expect(mockapp.instances[0].logger).to be_eql logger1
+    expect(mockapp.instances[1].logger).to be_eql logger2
   end
 
   it 'pass-through requests to nextapp' do
@@ -59,14 +66,18 @@ describe 'middleware' do
     expect(last_response.body).to be_eql 'OK'
     post '/'
     expect(last_response.body).to be_eql 'OK'
-    get '/api/fluentcounter'
+    get '/api/fluent_logger_counter/'
     expect(last_response.body).to be_eql 'OK'
-    post '/api/fluentcounter/test_logger'
+    post '/api/fluent_logger_counter/'
     expect(last_response.body).to be_eql 'OK'
   end
 
   it 'pass requests to FluentLoggerCounter::App on specific path' do
-    get '/api/fluentcounter/test_logger'
+    get '/api/fluent_logger_counter/stdout'
+    json = JSON.parse(last_response.body)
+    expect(json["buffer_size"]).to be_eql 0
+
+    get '/api/fluent_logger_counter/stderr'
     json = JSON.parse(last_response.body)
     expect(json["buffer_size"]).to be_eql 0
   end
